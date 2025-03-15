0b6a93d1feb9e2400aa80fc5f0f1a7577fb0a8b8
pggh-01202326 <pgghqwerty@gmail.com>
2025-03-15 15:40:24 +0700

    [update]

--- lab_02/cipher/transposition/transposition_cipher.py
+++ lab_02/cipher/transposition/transposition_cipher.py
@@ -12,12 +12,22 @@ class TranspositionCipher:
         return encrypted_text
     
     def decrypt(self, text, key):
-        decrypted_text = [''] * key
-        row, col = 0, 0
+        # Bước 1: Tạo mảng rỗng cho các hàng (với số hàng = len(text) // key)
+        num_of_rows = len(text) // key
+        num_of_extra_chars = len(text) % key  # Các ký tự thừa ở cột đầu
+        decrypted_text = [''] * num_of_rows
+        
+        col = 0
+        row = 0
         for symbol in text:
             decrypted_text[row] += symbol
-            col += 1
-            if col == key or (col == key - 1 and row >= len(text) % key):                
-                col = 0
-                row += 1
+            row += 1
+            # Kiểm tra nếu cột đã đầy hoặc nếu có ký tự thừa
+            if (row == num_of_rows and col < num_of_extra_chars):
+                row = 0
+                col += 1
+            if row == num_of_rows:  # Cột đã đầy, chuyển qua cột tiếp theo
+                row = 0
+                col += 1
+                
         return ''.join(decrypted_text)
